0fe2d7fd51c5ecbaeb70cb08bfea6bac6be7ef71
talwor <taylor1__@outlook.com>
2022-10-01 12:52:46 +1000

    everything but the token

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#token = MTAyNTQxNTI0NDEwNjk2OTE0OQ.G32Qid.pzTzo82Yl7Por3IoYPerMZ-F2wewlF7hgwTzDE
+#token = bruh
 #link = https://discord.com/api/oauth2/authorize?client_id=1025415244106969149&permissions=8&scope=bot
 #C:\Program Files\Python310\Scripts
 
